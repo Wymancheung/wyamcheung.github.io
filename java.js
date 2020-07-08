6440f299f390ee5e75b9c05beb4a8e50b8bb23b4
Wymancheung <wymancheung95@gmail.com>
2020-07-08 19:14:47 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,3 +1,7 @@
 <script>
 document.getElementById("demo").innerHTML = "My First JavaScript";
 </script>
+
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
