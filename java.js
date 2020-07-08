2e8500f9ac12d0b7ee435f3755cc63a320a336f7
Wymancheung <wymancheung95@gmail.com>
2020-07-08 19:17:45 +0800

    Update java.js

--- java.js
+++ java.js
@@ -4,4 +4,17 @@ document.getElementById("demo").innerHTML = "My First JavaScript";
 
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js">
+  // init controller
+var controller = new ScrollMagic.Controller();
+
+// create a scene
+new ScrollMagic.Scene({
+    duration: 100, // the scene should last for a scroll distance of 100px
+    offset: 50 // start this scene after scrolling for 50px
+})
+    .setPin('#my-sticky-element') // pins the element for the the scene's duration
+    .addTo(controller); // assign the scene to the controller
+
+  
+  </script>
